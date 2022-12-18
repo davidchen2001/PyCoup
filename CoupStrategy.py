970d8eb8786f29caa07279e1dd16b4cc1c93559c
Dioceus <chuwei.chen2001@gmail.com>
2022-12-18 15:26:15 -0500

    add function interfaces for strategy

---
+++ CoupStrategy.py
@@ -0,0 +1,14 @@
+def potentialToWin(player, strategy):
+    return 0
+
+def calculateCoinsPotential(player, strategy):
+    return 0
+
+def coinsToKillPotential(player, targetPlayer, strategy):
+    return 0
+
+def generateNextStrategy(player, strategy):
+    return 0
+
+def generateNextCardAction(player, strategy):
+    return 0
