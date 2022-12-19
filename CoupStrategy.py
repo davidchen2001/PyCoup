0cace0644c04c81e33f4678554c4c6349d9675dc
Dioceus <chuwei.chen2001@gmail.com>
2022-12-19 18:03:57 -0500

    add WinPotential function

--- CoupStrategy.py
+++ CoupStrategy.py
@@ -1,5 +1,27 @@
-def potentialToWin(player, strategies):
-    return 0
+def potentialToWin(player, players, strategies):
+    #strategies parameter?
+
+    totalPotential = 0
+    totalAgentCoinsToKillPotential = 0
+
+    for p in players:
+        if player != p and p.getIsPlayer() == True:
+            getCoinsPotential = calculateCoinsPotential(p, strategies)
+            playerCoinsToKillPotential = coinsToKillPotential(p, player, strategies)
+
+            #1 represents number of active cards. It will be 1 due to endgame scenario
+            playerPotential = 1 * (p.getCoins() + getCoinsPotential)/playerCoinsToKillPotential
+            totalPotential += playerPotential
+
+            agentPlayerCoinToKillPotential = coinsToKillPotential(player, p, strategies)
+            totalAgentCoinsToKillPotential += agentPlayerCoinToKillPotential
+    
+    agentCoinPotential = calculateCoinsPotential(player, strategies)
+    agentCoinsToKillPotential = totalAgentCoinsToKillPotential/(len(players))
+    agentPotential = 1 * (player.getCoins() + agentCoinPotential)/agentCoinsToKillPotential
+    totalPotential += agentPotential
+
+    return agentPotential/totalPotential
 
 def calculateCoinsPotential(player, strategy):
     return 0
@@ -10,14 +32,14 @@ def coinsToKillPotential(player, targetPlayer, strategy):
 def generateNextStrategy(player, strategies):
     return 0
 
-def generateNextCardAction(player, strategy):
+def generateNextCardAction(player, players, strategy):
     availableActions = player.getPossibleActions() #Need to implement a version of truthful vs non-truthful player
     bestUtility = -2
     bestAction = 0
 
     for action in availableActions:
         outcome = player.generateCardOutcome(action) #Need to implement hypothetical perform card action that doesn't actually change game state
-        actionUtility = potentialToWin(player, strategy)
+        actionUtility = potentialToWin(player, players, strategy)
         if actionUtility > bestUtility:
             bestUtility = actionUtility
             bestAction = action
